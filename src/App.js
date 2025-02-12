a031ee951afe84272173ebd28ea5487a42b99fba
Terreneitor2004 <23001440@galileo.edu>
2025-02-12 01:29:05 -0600

    los textos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Card } from "react-bootstrap";
+import { Container, Card, Button } from "react-bootstrap";
+
+const quotes = [
+  {
+    text: "Wdsdsdsd.",
+    author: "wkw8jj2",
+  },
+  {
+    text: "sdsdjsuw.",
+    author: "amamsadw",
+  },
+  {
+    text: "sdsds",
+    author: "weasd",
+  },
+  {
+    text: "If it is not right, do not do it, if it is not true, do not say it.",
+    author: "Marcus Aurelius",
+  },
+];
 
 export default function App() {
   const [time, setTime] = useState(new Date());
